1eba0598530558e84e1fc488feaf090c0ceeab75
Eric West <esw9999@gmail.com>
2015-04-20 00:26:30 -0500

    Adding inches conversion.

--- two/length_converter.ex
+++ two/length_converter.ex
@@ -1,5 +1,14 @@
-defmodule MeterToFootConverter do
- def convert(m) do
-   m * 3.28084
- end
+defmodule MeterToLengthConverter do
+  defmodule Feet do
+    def convert(m) do
+      m * 3.28084
+    end   
+  end
+
+  defmodule Inch do
+    def convert(m) do
+      m * 39.3701
+    end
+  end
 end
+
