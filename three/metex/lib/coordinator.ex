274a215c741f13cb3c572b0c93c09ba04195a964
Eric West <esw9999@gmail.com>
2015-04-26 00:25:36 -0500

    Use coordinator to send and sort temp responses

--- three/metex/lib/coordinator.ex
+++ three/metex/lib/coordinator.ex
@@ -2,7 +2,7 @@ defmodule Metex.Coordinator do
   def loop(results \\ [], results_expected) do
     receive do
       {:ok, result} ->
-        new_results = [results | results]
+        new_results = [result | results]
         if results_expected == Enum.count(new_results) do
           send self, :exit
         end
